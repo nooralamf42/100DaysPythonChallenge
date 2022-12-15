56a5ebeb04543603b3c46db32ef1116ed905593f
Noor Alam <90553481+nooralamf42@users.noreply.github.com>
2022-12-15 17:28:09 +0000

    learnt variables

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("Hello World!")
+#wap to print your input name's length
+
+name = input("Hello! can you type you name : ");
+namelength = len(name);
+print("your name have ",namelength, " lettors");
