d94d18064b3bd2889362f976cc96fd25ac3be273
Noor Alam <90553481+nooralamf42@users.noreply.github.com>
2023-01-01 13:58:26 +0000

    guess the number game

--- main.py
+++ main.py
@@ -1,123 +1,66 @@
-from art import logo,bust,lose,win,draw
-from replit import clear
+#Number Guessing Game
 import random
+from logo import logo
 
-start_true=True
-while start_true:
-  start = input("Do you to want to play?\nType 'y for yes or 'n' for no : ")
-  if start=="n":
-    start_true=False
-    clear()
-    print("\nHave a good day!")
-    
-  elif start!="n" and start!="y":
-    clear()
-    print("\nType corrent input letter!\n")
+print(logo)
+print("Welcome to Number Guessing Game!")
+print("I am thinking of a number between 1 to 100")
+print(
+    "NOTE : When you guess a number make sure you input a number not any letter or special character.\nHard difficulty will give you 5 guess chances and easy will give you 10 chance."
+)
 
-  else:
-    start_true=False
-       
-def black_jack():
-  """to start black jack game"""
-  clear()
-  print(logo)
-  sum_of_computer_cards = 0
-  sum_of_user_cards = 0
-  computer_cards = []
-  user_cards = []
-  
-  should_continue=2
-  while should_continue:
-      def card_picker():
-        """to pick random cards"""
-        cards=[11,2,3,4,5,6,7,8,9,10,10,10,10]
-        pick=random.randint(0,12)
-        card_picked = cards[pick]
-        if sum_of_user_cards>21 or sum_of_computer_cards>21 and card_picked==11:
-            card_picked=1    
-        return (card_picked)
-        
-      def cards_adder(cards):
-        """to add the values of cards"""
-        sum_of_cards=0
-        for add in cards:
-          sum_of_cards+=add
-        return sum_of_cards
-       
-      if should_continue==1:
-          give_cards=1
-      else:
-          give_cards=2
-      while give_cards:
-        if sum_of_computer_cards<17:
-          computer_cards.append(card_picker())
-        user_cards.append(card_picker())
-        give_cards-=1
+selected_number = random.randint(1, 100)
+# print("psst selected number is",selected_number)
+
+
+def ask_level():
+    level = input(
+        "\nChoose a difficulty level.\nType 'easy' for easy and 'hard' for hard : "
+    )
+    if level == "easy":
+        life = 10
+    elif level == "hard":
+        life = 5
+    else:
+        print("\nType a valid input!")
+        ask_level()
+
+    def check_guess(input_guess):
+        if input_guess > selected_number:
+            return 1
+        elif input_guess == selected_number:
+            return 0
+        else:
+            return -1
+
+    while life:
+        guess = int(input("\nType your guess : "))
+        checked_guess = check_guess(guess)
+        if checked_guess == 1:
+            life -= 1
+            print(f"  Guessed number is too high.(Remaining guess = {life}).")
+        elif checked_guess == 0:
+            break
+        else:
+            life -= 1
+            print(f"  Guessed number is too low.(Remaining guess = {life}).")
+
+    if life > 0:
+        print(
+            f"\nYOU WON !\n  Guessed Number = {guess}\n  Picked Number = {selected_number}"
+        )
+
+    elif life == 0:
+        print("\nYou LOSE 😥\n  You ran out of life.")
       
-      sum_of_computer_cards = cards_adder(computer_cards)
-      sum_of_user_cards = cards_adder(user_cards)
-
-      if sum_of_user_cards>21:
-        should_continue=False
-        print(f"{bust}\nYou went over you lose 😥.")
-        print(f"\n  Computer cards : {computer_cards}\n    Sum of computer cards is {sum_of_computer_cards}.")
-        print(f"  Your cards : {user_cards}\n    Sum of your cards is {sum_of_user_cards}.")
-                
-        continue_restart=True
-        while(continue_restart):
-          restart=(input("\nDo you want to play again?\nType 'y' for yes and 'n' for no: "))
-          if restart=="y":
-            black_jack()
-          elif(restart=="n"):
-            continue_restart=False
-            clear()
-            print("\nHave a good day!")
-          else:
-              print("\nType a valid input letter!")
-            
-      if(should_continue!=False):
-        print(f"\n  Computer's First Card : {computer_cards[0]}")
-        print(f"  Your cards : {user_cards}")
-        print(f"  Sum of your cards {sum_of_user_cards}\n")
-
-        ask_choice=True
-        while ask_choice:
-          choice=input("If you want to pick another card type 'p' and to show cards type 's' : ")
-          if choice=="p":
-            should_continue=1
-            ask_choice=False
-          elif choice!="s":
-            print("\nType a valid input letter!")
-          elif choice=="s":
-            ask_choice=False
-            if sum_of_computer_cards>21:
-              print(f"{win}\nComputer went over!!!\nYOU WON 😍.")
-              print(f"  Your cards : {user_cards}\n  Sum of your cards is {sum_of_user_cards}.")
-              print(f"  Computer cards : {computer_cards}\n  Sum of computer's card is {sum_of_computer_cards}.")
-            elif sum_of_computer_cards>sum_of_user_cards:
-              print(f"{lose}\nYou have lost the game 😥.")
-              print(f"  Your cards : {user_cards}\n  Sum of your cards is {sum_of_user_cards}.")
-              print(f"  Computer cards : {computer_cards}\n  Sum of computer's card is {sum_of_computer_cards}.")
-            elif sum_of_computer_cards==sum_of_user_cards:
-              print(f"{draw}\nThis match is Draw. Nobody wins 😶.")
-              print(f"  Your cards : {user_cards}\n  Sum of your cards is {sum_of_user_cards}.")
-              print(f"  Computer cards : {computer_cards}\n  Sum of computer's card is {sum_of_computer_cards}.")
-            else:             
-               print(f"{win}\nYOU WON 😍.")
-               print(f"  Your cards : {user_cards}\n  Sum of your cards is {sum_of_user_cards}.")
-               print(f"  Computer cards : {computer_cards}\n  Sum of computer's card is {sum_of_computer_cards}.")
-              
-            continue_restart=True
-            while(continue_restart):
-              restart=(input("\nDo you want to play again?\nType 'y' for yes and 'n' for no: "))
-              clear()
-              if restart=="y":
-                black_jack()
-              elif(restart=="n"):
-                continue_restart=False
-                should_continue=False
-                clear()
-                print("\nHave a good day!")                
-              else:
-                  print("\nType a valid input letter!\n")
-black_jack()
+    def restart_loop():
+      restart_game=input("\nDo you want to play again?\nType 'y' for yes and 'n' for no : ")
+      if restart_game=='y':
+        clear()
+        ask_level()
+      elif restart_game=='n':
+        print("\nHave a good day!")
+      else:
+        restart_loop()
+    restart_loop()
+ask_level()
